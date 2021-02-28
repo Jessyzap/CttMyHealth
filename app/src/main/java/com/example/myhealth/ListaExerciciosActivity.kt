d423d709be65277a0ca2d1bfa9579a8a0975bc64
Jessica <j.zaparolli.arq@gmail.com>
2021-02-28 13:58:08 -0300

    posição retrato

--- app/src/main/java/com/example/myhealth/ListaExerciciosActivity.kt
+++ app/src/main/java/com/example/myhealth/ListaExerciciosActivity.kt
@@ -1,42 +1,41 @@
 package com.example.myhealth
 
-import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myhealth.model.Exercicio
-import com.example.myhealth.model.Usuario
-import kotlinx.android.synthetic.main.activity_cadastro.*
 
 
 class ListaExerciciosActivity : AppCompatActivity() {
 
-//    private lateinit var foto: ImageView
-//    private lateinit var nome: TextView
+    private lateinit var foto: ImageView
+    private lateinit var nome: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_lista_exercicios)
 
+        foto = findViewById(R.id.fotoFixa)
+        nome = findViewById(R.id.nomeFixo)
+
+        val nomeRecebido = intent.getStringExtra("NOME")
+        nome.text = nomeRecebido
+
+        //val fotoRecebida = intent.extras?.get("IMAGEM") as Bitmap
+        //foto.setImageBitmap(fotoRecebida)
+
 
 //        val usuario = intent.extras?.get("USUARIO") as Usuario
 //
+//        usuario.foto?.let{
+//            imgUsuario.setImageBitmap(it)
+//        }
 //
-////        usuario.foto?.let{
-////            imgUsuario.setImageBitmap(it)
-////        }
-//
-//
-//        foto = findViewById(R.id.fotoFixa)
-//        nome = findViewById(R.id.nomeFixo)
-//
-//        //nome.text = usuario.nome
+//        nome.text = usuario.nome
 
 
         val lista: MutableList<Exercicio> = mutableListOf(
