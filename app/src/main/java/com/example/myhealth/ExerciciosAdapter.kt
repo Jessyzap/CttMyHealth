7e8d50bdda130b37eac35d189fede5ffbdc19738
Ana Arine <analucia.arine@gmail.com>
2021-03-02 00:58:21 -0300

    Intent lista

--- app/src/main/java/com/example/myhealth/ExerciciosAdapter.kt
+++ app/src/main/java/com/example/myhealth/ExerciciosAdapter.kt
@@ -13,7 +13,7 @@ import androidx.core.content.ContextCompat.startActivity
 import androidx.core.graphics.drawable.toDrawable
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myhealth.model.Exercicio
-
+import com.example.myhealth.model.Usuario
 
 
 class ExerciciosAdapter(private val listaExercicios: MutableList<Exercicio>) :
@@ -23,16 +23,6 @@ class ExerciciosAdapter(private val listaExercicios: MutableList<Exercicio>) :
     class ExercicioHolder(view: View) : RecyclerView.ViewHolder(view){
         val nomeExercicio: TextView = view.findViewById(R.id.txtExercicio)
         val fotoExercicio: ImageView = view.findViewById(R.id.imgExercicio)
-
-//        init{
-//            view.setOnClickListener{
-//            //    val position: Int = adapterPosition
-//
-//            val intent = Intent(view.context, ExercicioActivity::class.java)
-//                view.context.startActivity(intent)
-//                }
-////                   Toast.makeText(view.context, "Clicado", Toast.LENGTH_SHORT).show()
-//        }
 
 
     }
@@ -48,12 +38,22 @@ class ExerciciosAdapter(private val listaExercicios: MutableList<Exercicio>) :
         holder.fotoExercicio.setImageResource(listaExercicios[position].foto)
         holder.itemView.setOnClickListener(){
 
-//            Toast.makeText(it.context, "Deu certo", Toast.LENGTH_SHORT).show()
-             val novaTela = Intent(it.context, ExercicioActivity::class.java)
-             it.context.startActivity(novaTela)
+//            Toast.makeText(it.context, "Posição $position", Toast.LENGTH_SHORT).show()
+//             val novaTela = Intent(it.context, ExercicioActivity::class.java)
+//             it.context.startActivity(novaTela)
+            val destinoActivity = Intent(it.context, ExercicioActivity::class.java)
+            destinoActivity.putExtra("EXERCICIO", listaExercicios[position].foto)
+            destinoActivity.putExtra("NOME", listaExercicios[position].nomeExercicio)
+            it.context.startActivity(destinoActivity)
+            //it.context
         }
     }
 
+//    fun redirecionar(exercicio: Exercicio, holder: ExercicioHolder) {
+//
+//
+//
+//    }
 
     override fun getItemCount(): Int {
         return listaExercicios.size
--- app/src/main/java/com/example/myhealth/ListaExerciciosActivity.kt
+++ app/src/main/java/com/example/myhealth/ListaExerciciosActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myhealth
 
+import android.content.Intent
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -51,4 +52,5 @@ class ListaExerciciosActivity : AppCompatActivity() {
         rvExercicios.adapter = ExerciciosAdapter(lista)
         rvExercicios.layoutManager = LinearLayoutManager(this)
     }
+
 }
